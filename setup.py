a1ecea3f5d0f083bcf04acc728de34aeb6378b1e
Eugene <ti.eugene@gmail.com>
2022-05-22 16:28:09 +0300

    Initial import from pyqtpim

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pym_core',
+    version='0.0.4',
+    packages=['pym_core'],
+    package_dir={'': 'pym_core'},
+    url='https://github.com/tieugene/pym-core',
+    license='GPLv3',
+    author='TI_Eugene',
+    author_email='ti.eugene@gmail.com',
+    description='Python-based PIM (core)',
+)
